b17b65df58509d19242910d39264d943769aa4be
Usuário <Usurio@DESKTOP-KJUP1TH>
2024-12-08 13:11:07 -0300

    Add project files.

---
+++ Contact/dbo.People.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[ContactID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(100) NULL, 
+    [Company] NVARCHAR(100) NULL, 
+    [Telephone] NVARCHAR(100) NULL, 
+    [Email] NVARCHAR(100) NULL, 
+    [Client] BIT NULL, 
+    [LastCall] DATETIME NULL
+)
